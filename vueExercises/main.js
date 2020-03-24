cd92a7d70ba0948492b35dea6e945b363b6934f3
Heike Miersch <heike.miersch@hotmail.de>
2020-03-24 11:40:18 +0100

    just a couple of little reminders

---
+++ vueExercises/main.js
@@ -0,0 +1,15 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        isTrue: true,
+        message: "Hello Vue"
+    },
+    methods: {
+        toggleIsTrue: function () {
+            this.isTrue = !this.isTrue
+        },
+        methodThatReturnsSomething: function () {
+            return "Blahblah"
+        }
+    }
+});
