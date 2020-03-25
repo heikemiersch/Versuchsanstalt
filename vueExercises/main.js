4d38189fabf687b246c002a9462f4e147e90bbe4
Heike Miersch <heike.miersch@hotmail.de>
2020-03-25 09:54:16 +0100

    events

--- vueExercises/main.js
+++ vueExercises/main.js
@@ -2,7 +2,10 @@ const app = new Vue({
     el: "#app",
     data: {
         isTrue: true,
-        message: "Hello Vue"
+        message: "Hello Vue",
+        number: 30,
+        x: 0,
+        y: 0
     },
     methods: {
         // toggleIsTrue: function () {
@@ -11,5 +14,18 @@ const app = new Vue({
         // methodThatReturnsSomething: function () {
         //     return "Blahblah"
         // }
+        add: function (inc) {
+            this.number += inc
+        },
+        substract: function (dec) {
+            this.number -= dec
+        },
+        updateXY: function (event) {
+            this.x = event.offsetX
+            this.y = event.offsetY
+        },
+        createAlert() {
+            alert("alt + enter pressed")
+        }
     }
 });
